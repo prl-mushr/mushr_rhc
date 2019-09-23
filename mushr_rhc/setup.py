dc428d21084b81c02825a631018c5b517f8c68a7
Matthew Rockett <rockettm@cs.washington.edu>
2019-09-23 14:21:26 -0700

    ROS directory and library directory

---
+++ mushr_rhc/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='MuSHR RHC',
+    version='1.0',
+    description='MuSHR receding horizon control library.',
+    author='Matthew Rockett & Johan Michalove',
+    author_email='mushr@cs.washington.edu',
+    url='http://mushr.io',
+    packages=['librhc'],
+    requires=[
+        ''
+    ],
+)
