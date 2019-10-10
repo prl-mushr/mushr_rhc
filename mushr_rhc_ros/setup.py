2120ed5a82922e1146439840cfc05fe44e093c89
Matthew Rockett <rockettm@cs.washington.edu>
2019-10-10 12:56:17 -0700

    Run through black

--- mushr_rhc_ros/setup.py
+++ mushr_rhc_ros/setup.py
@@ -5,15 +5,8 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
-setup_args = generate_distutils_setup(packages=["mushr_rhc_ros"], package_dir={"": "src"})
-
-setup(
-    install_requires=[
-        "numpy",
-        "torch",
-        "networkx",
-        "scipy",
-        "sklearn",
-    ],
-    **setup_args
+setup_args = generate_distutils_setup(
+    packages=["mushr_rhc_ros"], package_dir={"": "src"}
 )
+
+setup(install_requires=["numpy", "torch", "networkx", "scipy", "sklearn"], **setup_args)
