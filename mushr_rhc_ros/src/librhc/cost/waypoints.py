6f0cd717038f0610f51993696cfd8db140fcb07c
Madison Doerr <mcdoerr@uw.edu>
2021-05-13 18:24:55 -0700

    calculate cross track error

--- mushr_rhc_ros/src/librhc/cost/waypoints.py
+++ mushr_rhc_ros/src/librhc/cost/waypoints.py
@@ -2,7 +2,11 @@
 # License: BSD 3-Clause. See LICENSE.md file in root directory.
 
 import torch
-
+import rospy
+import tf
+import numpy as np
+from geometry_msgs.msg import PoseStamped
+from nav_msgs.msg import Path
 
 class Waypoints:
     NPOS = 3  # x, y, theta
@@ -15,6 +19,18 @@ class Waypoints:
 
         self.world_rep = world_rep
         self.goal = None
+        self.path = None
+        self.car_pose = None
+
+        self.path_sub = rospy.Subscriber(
+            rospy.get_param("~path_topic"), Path, self.path_cb, queue_size=1
+        )
+        self.start_sub = rospy.Subscriber(
+            rospy.get_param("~car_pose"),
+            PoseStamped,
+            self.car_pose_cb,
+            queue_size=1,
+        )
 
         self.viz_rollouts = self.params.get_bool("debug/flag/viz_rollouts", False)
         self.n_viz = self.params.get_int("debug/viz_rollouts/n", -1)
@@ -74,7 +90,36 @@ class Waypoints:
             .sum(dim=1)
         )
 
+        cross_track_error = None
+
+        # calculate cross track error
+        if self.path is not None and self.car_pose is not None:
+            # calculate lookahead
+            distance_lookahead = 5
+
+            # calculate closest index to car position
+            diff = np.sqrt(((self.path[:,0] - self.car_pose[0]) ** 2) + ((self.path[:,1] - self.car_pose[1]) ** 2))
+            index = np.argmin(diff)
+
+            while index < len(self.path) - 1 and diff[index] < distance_lookahead:
+                index += 1
+
+            if abs(diff[index - 1] - distance_lookahead) < abs(diff[index] - distance_lookahead):
+                index -= 1
+
+            lookahead = self.path[index]
+
+            # ect = -(x - x_ref)sin(theta_ref) + (y - y_ref)cos(theta_ref)
+            cross_track_error = -(poses[:, -1, 0] - lookahead[0]) * np.sin(lookahead[2]) + (poses[:, -1, 1] - lookahead[1]) * np.cos(lookahead[2])
+
+            # add weights
+            cross_track_error *= 10.0
+
         result = collision_cost.add(obs_dist_cost).add(smoothness)
+
+        if self.path is not None and self.car_pose is not None:
+            print("adding cross track error")
+            result.add(cross_track_error)
 
         colliding = collision_cost.nonzero()
         result[colliding] = 1000000000
@@ -132,3 +177,37 @@ class Waypoints:
     def set_goal(self, goal):
         self.goal = goal
         return True
+
+    def path_cb(self, msg):
+        path = []
+        for pose_stamped in msg.poses:
+            point = pose_stamped.pose.position
+            orientation = pose_stamped.pose.orientation
+            theta = tf.transformations.euler_from_quaternion(
+                [
+                    orientation.x,
+                    orientation.y,
+                    orientation.z,
+                    orientation.w,
+                ]
+            )
+            path.append(np.array([point.x, point.y, theta[2]]))
+        self.path = np.array(path)
+
+    def car_pose_cb(self, msg):
+        """
+        Record the new car position as the start.
+
+        Attributes:
+            msg (geometry_msgs/PoseStamped): goal position
+
+        """
+        theta = tf.transformations.euler_from_quaternion(
+            [
+                msg.pose.orientation.x,
+                msg.pose.orientation.y,
+                msg.pose.orientation.z,
+                msg.pose.orientation.w,
+            ]
+        )
+        self.car_pose = [msg.pose.position.x, msg.pose.position.y, theta[2]]
